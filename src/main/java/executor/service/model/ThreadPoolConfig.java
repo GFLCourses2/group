8ecdc3201f6ae3654d8e98c333161d6c90e3e806
lisak <lisk.semen@gmail.com>
2022-11-07 18:29:56 +0100

    Created test for ThreadPoolConfig, added constructors for it

--- src/main/java/executor/service/model/ThreadPoolConfig.java
+++ src/main/java/executor/service/model/ThreadPoolConfig.java
@@ -9,18 +9,25 @@ class ThreadPoolConfig {
 	private Integer corePoolSize;
 	private Long keepAliveTime;
 
+	public ThreadPoolConfig() {}
+
+	public ThreadPoolConfig (Integer corePoolSize, Long keepAliveTime) {
+		this.corePoolSize = corePoolSize;
+		this.keepAliveTime = keepAliveTime;
+	}
+
 	public Integer getCorePoolSize() {
 		return corePoolSize;
 	}
-	
+
 	public void setCorePoolSize(Integer corePoolSize) {
 		this.corePoolSize = corePoolSize;
 	}
-	
+
 	public Long getKeepAliveTime() {
 		return keepAliveTime;
 	}
-	
+
 	public void setKeepAliveTime(Long keepAliveTime) {
 		this.keepAliveTime = keepAliveTime;
 	}
@@ -35,7 +42,7 @@ class ThreadPoolConfig {
 		if (o == this) return true;
 		if (!(o instanceof ThreadPoolConfig)) return false;
 		ThreadPoolConfig other = (ThreadPoolConfig) o;
-		
+
 		return Objects.equals(this.corePoolSize, other.corePoolSize) && Objects.equals(this.keepAliveTime, other.keepAliveTime);
 	}
 
