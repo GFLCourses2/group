0c0cfb38b202aec782f12973e16e25d6714ce4c9
Semion <lisk.semen@gmail.com>
2022-11-07 19:35:17 +0200

    Revert "Thread pool config test"

--- src/main/java/executor/service/model/ProxyConfigHolder.java
+++ src/main/java/executor/service/model/ProxyConfigHolder.java
@@ -2,47 +2,47 @@ package executor.service.model;
 
 import java.util.Objects;
 
-public class ProxyConfigHolder {
+public class ProxyConfigHolder{
     private ProxyNetworkConfig proxyNetworkConfig;
     private ProxyCredentials proxyCredentials;
-
-    public ProxyConfigHolder() {
-
+    
+    public ProxyConfigHolder(){
+        
     }
-
-    public ProxyConfigHolder(ProxyNetworkConfig proxyNetworkConfig, ProxyCredentials proxyCredentials) {
+    
+    public ProxyConfigHolder(ProxyNetworkConfig proxyNetworkConfig, ProxyCredentials proxyCredentials){
         this.proxyNetworkConfig = proxyNetworkConfig;
         this.proxyCredentials = proxyCredentials;
     }
-
-    public void setProxyNetworkConfig(ProxyNetworkConfig proxyNetworkConfig) {
+    
+    public void setProxyNetworkConfig(ProxyNetworkConfig proxyNetworkConfig){
         this.proxyNetworkConfig = proxyNetworkConfig;
     }
-
-    public void setProxyCredentials(ProxyCredentials proxyCredentials) {
+    public void setProxyCredentials(ProxyCredentials proxyCredentials){
         this.proxyCredentials = proxyCredentials;
     }
-
-    public ProxyNetworkConfig getProxyNetworkConfig() {
+    
+    public ProxyNetworkConfig getProxyNetworkConfig(){
         return proxyNetworkConfig;
     }
-
-    public ProxyCredentials getProxyCredentials() {
+    
+    public ProxyCredentials getProxyCredentials(){
         return proxyCredentials;
     }
 
 
+
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ProxyConfigHolder)) return false;
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof ProxyConfigHolder)) return false;
         ProxyConfigHolder proxyConfHold = (ProxyConfigHolder) o;
         return Objects.equals(proxyNetworkConfig, proxyConfHold.proxyNetworkConfig) &&
-                Objects.equals(proxyCredentials, proxyConfHold.proxyCredentials);
+            Objects.equals(proxyCredentials, proxyConfHold.proxyCredentials);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode(){
         return Objects.hash(proxyNetworkConfig, proxyCredentials);
     }
 
