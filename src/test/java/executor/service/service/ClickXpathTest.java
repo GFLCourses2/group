6461a52be48077e93e1dc0ea9a5a09c55237e2dd
Potseluiko Vitalii <vetaliy063@gmail.com>
2022-11-18 10:29:52 +0200

    Changed ClickXpathTest.

--- src/test/java/executor/service/service/ClickXpathTest.java
+++ src/test/java/executor/service/service/ClickXpathTest.java
@@ -6,12 +6,8 @@ import executor.service.model.Step;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ClickXpathTest extends TestCase {
@@ -33,10 +29,9 @@ public class ClickXpathTest extends TestCase {
     public void testStep() throws InterruptedException {
         WebDriver webDriver = webDriverFactory.create();
         Step step = new Step("clickXpath", "//*[@id=\"gb\"]/div/div[1]/div/div[1]/a");
-        StepExecution clickXpath = new ClickXpath();
         webDriver.get("https://www.google.com.ua/");
         TimeUnit.SECONDS.sleep(4L);
-        clickXpath.step(webDriver, step);
+        stepExecution.step(webDriver, step);
         TimeUnit.SECONDS.sleep(4L);
         webDriver.quit();
     }
