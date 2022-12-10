83d51683e0a05c986ff2922dde2febf5450eaf25
VassBassApp <vassbassapp@gmail.com>
2022-12-10 20:57:21 +0200

    Spring migration: - ThreadPoolConfig - WebDriverConfig
    Lombok clearing:
    - ProxyConfigHolder
    - ProxyCredentials
    - ProxyNetworkConfig
    
    Added util for simplify work with properties:
    - PropertiesHelper

--- src/main/java/executor/utils/PropertiesHelper.java
+++ src/main/java/executor/utils/PropertiesHelper.java
@@ -1,12 +1,13 @@
 package executor.utils;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public class PropertiesHelper {
-    public static Properties readProperties(String path) {
+    public static Properties readProperties(@Nonnull String path) {
         Properties properties = new Properties();
         try {
             properties.load(Files.newInputStream(Paths.get(path)));
@@ -16,17 +17,17 @@ public class PropertiesHelper {
         }
     }
 
-    public static long getPropertyLongValue(Properties properties, String key, long defValue) {
+    public static long getPropertyLongValue(@Nonnull Properties properties, @Nonnull String key, long defValue) {
         try {
-            return Long.parseLong(properties.getProperty(key));
+            return Long.parseLong(properties.getProperty(key, String.valueOf(defValue)));
         } catch (NumberFormatException e) {
             return defValue;
         }
     }
 
-    public static int getPropertyIntValue(Properties properties, String key, int defValue) {
+    public static int getPropertyIntValue(@Nonnull Properties properties, @Nonnull String key, int defValue) {
         try {
-            return Integer.parseInt(properties.getProperty(key));
+            return Integer.parseInt(properties.getProperty(key, String.valueOf(defValue)));
         } catch (NumberFormatException e) {
             return defValue;
         }
