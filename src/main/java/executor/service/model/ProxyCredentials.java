6706b3f2637a3aab0ed8b747daf3235ebb566d98
VassBassApp <vassbassapp@gmail.com>
2022-12-11 21:36:16 +0200

    Fix

--- src/main/java/executor/service/model/ProxyCredentials.java
+++ src/main/java/executor/service/model/ProxyCredentials.java
@@ -2,11 +2,9 @@ package executor.service.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
-@Component
 public class ProxyCredentials {
     private String username;
     private String password;
--- src/main/java/executor/service/model/ProxyNetworkConfig.java
+++ src/main/java/executor/service/model/ProxyNetworkConfig.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
-@Component
 public class ProxyNetworkConfig {
     @JsonProperty("hostName")
     @JsonAlias("hostname")
