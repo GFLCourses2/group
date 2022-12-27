9599e6b218e41eb6b8898ec9dd6449aae091897f
lisak <lisk.semen@gmail.com>
2022-12-27 22:49:11 +0100

    Fixed Logger issue

--- src/main/java/executor/service/app/AppLauncher.java
+++ src/main/java/executor/service/app/AppLauncher.java
@@ -2,6 +2,8 @@ package executor.service.app;
 
 import executor.service.service.execution.executionservice.ParallelFlowExecutorService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class AppLauncher implements CommandLineRunner {
     private final ParallelFlowExecutorService parallelFlowExecutorService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(AppLauncher.class);
     @Override
     public void run(String... args) throws Exception {
         parallelFlowExecutorService.run();
